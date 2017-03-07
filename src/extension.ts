f4014e5392ebd17c3702724ec83bb4d152714359
Zim Kalinowski <zikalino@microsoft.com>
2017-03-07 17:00:32 +0800

    creating docker menu

--- src/extension.ts
+++ src/extension.ts
@@ -17,14 +17,19 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable1 = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World! XXX');
     });
 
-    context.subscriptions.push(disposable);
+    let disposable2 = vscode.commands.registerCommand('extension.openMainMenu', () => {
+        vscode.window.showInformationMessage('Main Menu Shall Be Opened Here!');
+    });
+
+    context.subscriptions.push(disposable1);
+    context.subscriptions.push(disposable2);
 
     checkDockerInstall().then(installed => {
         if (installed) {
@@ -36,6 +41,13 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
     });
+
+
+    var item = vscode.window.createStatusBarItem();
+
+    item.text = "Docker";
+    item.command = "extension.openMainMenu";
+    item.show();
 
 }
 
