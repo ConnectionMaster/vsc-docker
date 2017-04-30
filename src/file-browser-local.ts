0a2273c2a7d9c8bb23c20cafb5662e3ad9665a3a
Zim Kalinowski <zikalino@microsoft.com>
2017-04-30 08:40:29 +0800

    recursively deleting directory in local filesystem

--- src/file-browser-local.ts
+++ src/file-browser-local.ts
@@ -15,7 +15,21 @@ export class FileBrowserLocal extends FileBrowser
         }
 
         try {
-            fs.unlinkSync(this.m_CurrentDirectory + '/' + name);
+            var deleteFolderRecursive = function(path) {
+                if( fs.existsSync(path) ) {
+                    fs.readdirSync(path).forEach(function(file,index){
+                    var curPath = path + "/" + file;
+                    if(fs.lstatSync(curPath).isDirectory()) {
+                        deleteFolderRecursive(curPath);
+                    } else {
+                        fs.unlinkSync(curPath);
+                    }
+                    });
+                    fs.rmdirSync(path);
+                }
+            };
+
+            deleteFolderRecursive(this.m_CurrentDirectory + '/' + name);
         } catch (e) {
             console.log(e);
         }
