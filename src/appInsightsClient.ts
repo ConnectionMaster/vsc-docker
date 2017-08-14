4ded88514ed3b05f19edd4967c8ddbe07549663e
zikalino <zikalino@microsoft.com>
2017-08-14 11:18:56 +0800

    added app insights

--- src/appInsightsClient.ts
+++ src/appInsightsClient.ts
@@ -5,11 +5,8 @@ import appInsights = require("applicationinsights");
 
 export class AppInsightsClient {
     public static sendEvent(eventName: string, properties?: { [key: string]: string; }): void {
-        if (this._enableTelemetry) {
-            this._client.trackEvent(eventName, properties);
-        }
+        this._client.trackEvent(eventName, properties);
     }
 
-    private static _client = appInsights.getClient("067ba7db-9013-4f94-9d48-9338459bf259");
-    private static _enableTelemetry = Utility.getConfiguration().get<boolean>("enableTelemetry");
+    private static _client = appInsights.getClient("78bee696-3c3e-4bbc-b9c3-3cde845a6eee");
 }
